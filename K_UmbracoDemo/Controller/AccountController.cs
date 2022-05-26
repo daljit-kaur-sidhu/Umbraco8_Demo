49162d6ff66e6c506f1008f244fb72af40bc2a4b
Daljit Kaur <daljit.kaur@smartsource.eu>
2022-05-26 17:11:07 +0530

    Umbraco 8: Member login demo & db backup file.

---
+++ K_UmbracoDemo/Controller/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using K_UmbracoDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Services;
+using Umbraco.Web.Mvc;
+
+namespace K_UmbracoDemo.Controller
+{
+    public class AccountController : SurfaceController
+    {
+        private IMemberService _memberService;
+        private IMemberTypeService _memberTypeService;
+        public AccountController(IMemberService memberService, IMemberTypeService memberTypeService)
+        {
+            _memberService = memberService;
+            _memberTypeService = memberTypeService;
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid) {
+                var user = _memberService.GetByEmail(loginModel.Email);
+                //var isValid = Members.IsLoggedIn();
+                var isValid = Members.Login(loginModel.Email, loginModel.Password);
+                if (user != null && isValid) {
+                    var userRole = Members.GetUserRoles(loginModel.Email);
+                    if (userRole.Contains("Admin")) {
+                        return RedirectToUmbracoPage(1072);
+                    }
+                    if (userRole.Contains("User"))
+                    {
+                        return RedirectToUmbracoPage(1062);
+                    }
+                }
+            }
+            return RedirectToCurrentUmbracoUrl();
+        }
+    }
+}
---
+++ K_UmbracoDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("K_UmbracoDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("K_UmbracoDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("27e00789-43d2-45c2-9b18-9efe0e294369")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
